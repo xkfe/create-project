cd2cc79202813c2c80ec70b518eb38d27247d957
xkfe <xkfe16@gmail.com>
2024-09-01 13:52:21 +0800

    chore(vue-data-visual): 新增unplugin-icons、plugin-vue-jsx、vite-plugin-vue-devtools plugins

--- templates/vue-data-visual/vite.config.ts
+++ templates/vue-data-visual/vite.config.ts
@@ -2,13 +2,17 @@
  * @Author: xkfe
  * @Date: 2024-08-31 20:46:06
  * @LastEditors: xkfe
- * @LastEditTime: 2024-09-01 09:36:37
+ * @LastEditTime: 2024-09-01 13:50:25
  * @Description: vite配置文件
  */
+import { URL, fileURLToPath } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import Icons from 'unplugin-icons/vite'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -17,10 +21,11 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       vue(),
+      vueJsx(),
       UnoCSS(),
       // https://github.com/unplugin/unplugin-auto-import 配置自动导入 vue相关函数
       AutoImport({
-        imports: ['vue'],
+        imports: ['vue', 'vue-router'],
         dirs: [
           'src/stores/modules/*',
           'src/components/*',
@@ -28,7 +33,17 @@ export default defineConfig(({ mode }) => {
         ],
         dts: 'types/auto-imports.d.ts',
       }),
+      // 引入 iconfont
+      Icons({
+        autoInstall: true,
+      }),
+      VueDevTools(),
     ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
     envDir: 'env',
     server: {
       port: env.VITE_PORT,
