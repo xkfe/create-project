805565fdb7a0516eba254f457bd627d2ce7ff736
xkfe <xkfe16@gmail.com>
2024-09-13 17:55:24 +0800

    chore: 抽出一份格式化配置文件作为模板参考

---
+++ templates/lint-format/eslint.config.js
@@ -0,0 +1,11 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  formatters: true,
+  unocss: true,
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'no-restricted-syntax': 'off',
+  },
+})
